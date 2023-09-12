4687b782b1a169ec698a34b81e77244f0f046f57
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-09-12 21:43:13 +0900

    updated

--- process.php
+++ process.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 set_time_limit(0);
 
 if (isset($_POST['url']) && isset($_POST['format'])) {
@@ -8,17 +12,34 @@ if (isset($_POST['url']) && isset($_POST['format'])) {
 
     $interpreterPath = "python";
 
-    $command = "$interpreterPath ./python/download.py $url $format";
+    $command = "$interpreterPath ./python/download.py $url $format 2>&1";
     $output = shell_exec($command);
+    $lines = explode(PHP_EOL, $output);
+    $output = end($lines); // 最後の行だけを取得
+
 
     if ($output !== null) {
         $output = trim($output);
     }
 
-    if ($output && file_exists($output)) {
-        header("Location: index.php?file=" . urlencode($output));
+
+    // Windowsの場合、パスのスラッシュの方向を変更する
+    $corrected_output = str_replace('\\', '/', $output);
+    $corrected_document_root = str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT']);
+
+    if ($output !== null) {
+        $output = trim($corrected_output);
+    }
+
+    $relative_path = str_replace($corrected_document_root, '', $output);
+    $web_accessible_path = ltrim($relative_path, '/');
+
+    if ($output && file_exists($corrected_output)) {
+        header("Location: index.php?file=" . urlencode($web_accessible_path) . "&format=" . $_POST['format']);
+        exit;  // リダイレクト後にスクリプトの実行を停止
     } else {
-        echo "エラーが発生しました: " . $output;
+        echo "コマンドの実行: " . htmlspecialchars($command) . "<br>"; // コマンドの内容を表示
+        echo "エラーが発生しました: " . htmlspecialchars($output); // エラーメッセージも表示
     }
 }
 
--- python/download.py
+++ python/download.py
@@ -1,7 +1,21 @@
+import os
 import sys
 import yt_dlp
 
 def download_video(url, format):
+
+    def hook(d):
+        if d['status'] == 'finished':
+            # 出力ファイルの拡張子を設定
+            ext = 'mp3' if format == 'mp3' else 'mp4'
+            # ファイル名から品質コード部分を削除
+            filename = d['filename'].rsplit('.', 1)[0]
+            filename = filename.rsplit('.f', 1)[0]  # ここで品質コードを削除
+            filename += '.' + ext  # 正しい拡張子を追加
+            print("\n" + filename)
+
+
+
     if format == "mp3":
         postprocessors = [{
             'key': 'FFmpegExtractAudio',
@@ -14,17 +28,16 @@ def download_video(url, format):
         }]
 
     ydl_opts = {
-        'format': 'bestaudio' if format == 'mp3' else 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-        'outtmpl': './python/downloads/%(id)s.%(ext)s',
-        'postprocessors': postprocessors,
-        'quiet': True,      # Make yt_dlp quiet
-        'no_warnings': True, # Do not print out warnings
-        'progress_hooks': [hook],
-    }
+            'format': 'bestaudio' if format == 'mp3' else 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+            'outtmpl': os.path.abspath('./python/downloads/%(id)s.%(ext)s'),  # full path
+            'postprocessors': postprocessors,
+            'quiet': True,      # Make yt_dlp quiet
+            'no_warnings': True, # Do not print out warnings
+            'progress_hooks': [hook],
+            'noplaylist': True,  # Prevent downloading of playlists
+            'nopart': True       # Do not create .part files
+        }
 
-    def hook(d):
-        if d['status'] == 'finished':
-            print(d['filename'])
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
